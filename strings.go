be2cde06e291c060e5c999475ac84c36e4d816c4
joemuhuang <huangxing654123@163.com>
2021-09-24 09:43:14 +0800

    feat(strings): add strings func

---
+++ strings.go
@@ -0,0 +1,34 @@
+package go_utils
+
+import (
+	"fmt"
+	"math/rand"
+	"strings"
+	"time"
+)
+
+//RandomStr 随机生成字符串
+// length int 生成字符串的长度
+func RandomStr(length int) string {
+	str := "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	bytes := []byte(str)
+	result := []byte{}
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	for i := 0; i < length; i++ {
+		result = append(result, bytes[r.Intn(len(bytes))])
+	}
+	return string(result)
+}
+
+//GenValidateCode 生成随机验证码
+func GenValidateCode(width int) string {
+	numeric := [10]byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+	r := len(numeric)
+	rand.Seed(time.Now().UnixNano())
+
+	var sb strings.Builder
+	for i := 0; i < width; i++ {
+		_, _ = fmt.Fprintf(&sb, "%d", numeric[rand.Intn(r)])
+	}
+	return sb.String()
+}
